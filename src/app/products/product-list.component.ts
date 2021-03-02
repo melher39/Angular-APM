279ff44058572a463b332eadd39aa8bfb58b9bdf
Melvin Hernandez <melher39@gmail.com>
2021-03-02 15:13:13 -0600

    learned how to go about filtering a list based on user input. we used class setters and getters to accomplish this. these were new terms for me but I got the basics of it. end of module 7

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -7,11 +7,26 @@ import { IProduct } from "./product";
     styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = "cart";
+    }
     pageTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = "cart";
+    // listFilter: string = "cart";
+    _listFiler: string;
+    // when the data binding needs the value, the getter is called and the value is retrieved
+    get listFilter(): string {
+        return this._listFiler;
+    }
+    // when the user modifies the value, the data binding calls the setter, passing in the changed value
+    set listFilter(value: string) {
+        this._listFiler = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             "productId": 1,
@@ -34,6 +49,11 @@ export class ProductListComponent implements OnInit {
             "imageUrl": "assets/images/garden_cart.png"
         }
     ];
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
