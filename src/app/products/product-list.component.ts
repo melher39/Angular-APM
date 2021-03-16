7d7c67835a1ff432565d6a7fef54d5c0080578bc
Melvin Hernandez <melher39@gmail.com>
2021-03-16 15:07:15 -0500

    injected the product service into our product list component. seems a bit overcomplicated to just import a list of products. instead of hard coding the list of products, we are retrieving them from the service that returns an array of objects for us to use

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: "pm-products",
@@ -7,9 +8,7 @@ import { IProduct } from "./product";
     styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = "cart";
+    constructor(private productService: ProductService) {
     }
     pageTitle: string = "Product List";
     imageWidth: number = 50;
@@ -27,28 +26,7 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2019",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        }
-    ];
+    products: IProduct[] = [];
 
     onRatingClicked(message: string): void {
         this.pageTitle = `Product List: ${message}`;
@@ -64,6 +42,7 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        console.log("in ngOnInit");
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 }
