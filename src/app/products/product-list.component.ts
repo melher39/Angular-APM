adb5df5ccb8d4657ae4a27ef454e39fb592e188e
Melvin Hernandez <melher39@gmail.com>
2021-03-23 15:10:13 -0500

    successfully subscribed and unsubscribed to an http request service. app is still working correctly. a bit overly complicated just to retrieve some data but I do like the unsubscribe method. that's pretty straightforward and easy to use

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -7,7 +8,7 @@ import { ProductService } from "./product.service";
     templateUrl: "./product-list.component.html",
     styleUrls: ["./product-list.component.css"]
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     // service
     constructor(private productService: ProductService) {
     }
@@ -15,7 +16,9 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    // listFilter: string = "cart";
+    errorMessage: string = "";
+    sub!: Subscription;
+
     _listFiler: string;
     // when the data binding needs the value, the getter is called and the value is retrieved
     get listFilter(): string {
@@ -43,7 +46,16 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 }
