5f18c575c78468ddd729e22f96d1416fb46f93a0
Melvin Hernandez <melher39@gmail.com>
2021-04-01 12:13:19 -0500

    added the WelcomeComponent to our declarations array. will be used as a page to demonstrate routing. also changed some single quotes to double

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms"
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
 import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 import { StarComponent } from "./shared/star.component";
+import { ProductDetailComponent } from "./products/product-detail.component";
+import { WelcomeComponent } from "./home/welcome.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
