afb33b077fad502c3d11ae15ceee284fe037c681
Melvin Hernandez <melher39@gmail.com>
2021-04-14 18:39:06 -0500

    for a small hw assignment, I was able to retrieve the product's details from the json instead of hard coding it. it may not be in the correct or desired format, but it works for now. now when you click on any product, it will take you to its description with an actual list. when you click back, it will take you back to the overall product list

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   templateUrl: "./product-detail.component.html",
@@ -9,23 +11,24 @@ import { IProduct } from "./product";
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
   product: IProduct | undefined;
+  sub!: Subscription;
 
-  constructor(private route: ActivatedRoute,
-    private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
     const id: number = Number(this.route.snapshot.paramMap.get("id"));
     this.pageTitle += `: ${id}`;
-    this.product = {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    }
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        const product = products.filter(p => p.productId === id)[0];
+        this.product = product;
+      },
+      error: err => console.error(err)
+    });
   }
 
   onBack(): void {
